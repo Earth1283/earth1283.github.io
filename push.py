c0dc7607c9d2687429682104799c21ca3cc6c8dc
Earth1283 <142399154+Earth1283@users.noreply.github.com>
2025-07-11 13:43:10 +0000

    fixed push

--- push.py
+++ push.py
@@ -2,7 +2,7 @@ import os
 os.system("git add .")
 print("Staged all changes.")
 text = str(input("What is the commit msg? \n").strip())
-os.system(f"git commit -m {text}")
+os.system(f"git commit -m \"{text}\"")
 print("Committed changes with message:", text)
 os.system("git push origin main")
 print("Pushed changes to origin/main branch.")
